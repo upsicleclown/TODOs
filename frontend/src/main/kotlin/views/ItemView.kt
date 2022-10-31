3264069dbaed385dea90240ecbf13fa9918637e6
Moe Amadou <moeamadou@Moes-MacBook-Pro.local>
2022-10-31 19:16:41 -0400

    Add change listener for textField focus to hide delete button

--- frontend/src/main/kotlin/views/ItemView.kt
+++ frontend/src/main/kotlin/views/ItemView.kt
@@ -1,6 +1,7 @@
 package views
 
 import controllers.GroupViewController
+import javafx.beans.value.ChangeListener
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.scene.control.Button
@@ -8,19 +9,36 @@ import javafx.scene.control.ListCell
 import javafx.scene.control.TextField
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
+import javafx.scene.layout.BorderPane
 import models.Item
 
 class ItemView(private val controller: GroupViewController): ListCell<Item>() {
+        private val root = BorderPane()
         private val textField = TextField()
         private val deleteButton = Button("x")
 
         init {
+            root.left = deleteButton
+            root.center = textField
+
+            graphic = root
+
+
+            /* region textField setup */
+            // hide delete button while textField is focused
+            textField.focusedProperty().addListener(ChangeListener { _, _, newValue ->
+                when (newValue) {
+                    true -> focusItem()
+                    false -> unfocusItem()
+                }
+            })
+
+            // when unfocusing a textField, save changes
             textField.onAction = EventHandler { _: ActionEvent? ->
-                commitEdit(
-                    item
-                )
+                commitEdit(item)
             }
 
+            // when the ESC key is pressed, undo staged changes
             textField.addEventFilter(
                 KeyEvent.KEY_RELEASED
             ) { e: KeyEvent ->
@@ -28,23 +46,34 @@ class ItemView(private val controller: GroupViewController): ListCell<Item>() {
                     cancelEdit()
                 }
             }
+            /* end region textField setup */
         }
+
+    fun focusItem() {
+       root.left = null
+    }
+
+    fun unfocusItem() {
+        root.left = deleteButton
+    }
 
     override fun updateItem(item: Item?, empty: Boolean) {
         super.updateItem(item, empty)
         if (empty) {
-            text = null
             graphic = null
-        } else if (isEditing) {
+            return
+        }
+
+        graphic = root
+        if (isEditing) {
             textField.text = item?.title
-            text = null
-            graphic = textField
+
         } else {
             if (item != null) {
-                text = item.title
-                graphic = deleteButton
+                textField.text = item.title
                 deleteButton.setOnAction {
                     controller.deleteItem(item)
+                    println("deleted")
                 }
             }
         }
@@ -52,17 +81,17 @@ class ItemView(private val controller: GroupViewController): ListCell<Item>() {
 
     override fun startEdit() {
         super.startEdit()
+        // hide delete button
+        root.left = null
+
         textField.text = item?.title
-        text = null
-        graphic = textField
         textField.selectAll()
         textField.requestFocus()
     }
 
     override fun cancelEdit() {
         super.cancelEdit()
-        text = item?.title
-        graphic = null
+        textField.text = item?.title
     }
 
     override fun commitEdit(item: Item?) {
@@ -70,8 +99,6 @@ class ItemView(private val controller: GroupViewController): ListCell<Item>() {
         if (item != null) {
             val originalItem = item.copy()
             item.title = textField.text
-            text = textField.text
-            graphic = null
             controller.editItem(item, originalItem)
         }
     }
