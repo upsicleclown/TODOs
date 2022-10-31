5194e474b329a96e8e731d002d8415b380b7a762
dakadabra <david.a.khazzam@gmail.com>
2022-10-31 16:39:59 -0400

    cleaned up code

--- frontend/src/main/kotlin/views/SidepaneView.kt
+++ frontend/src/main/kotlin/views/SidepaneView.kt
@@ -27,7 +27,6 @@ class SidepaneView(sidepaneController: SidepaneController) : VBox() {
                 private val textField = TextField()
 
                 init {
-                    // TODO: How do I do this better?
                     this.onMousePressed = EventHandler { sidepaneController.focusGroup(item) }
                     textField.onAction = EventHandler { _: ActionEvent? ->
                         commitEdit(
@@ -78,7 +77,7 @@ class SidepaneView(sidepaneController: SidepaneController) : VBox() {
                 override fun cancelEdit() {
                     super.cancelEdit()
                     text = item?.name
-                    graphic = null
+                    graphic = deleteButton
                 }
 
                 override fun commitEdit(group: Group?) {
