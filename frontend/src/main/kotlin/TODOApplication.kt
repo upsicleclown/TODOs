e0c49d5a41191180e628ee9a18fb8c15ae9fffc3
dakadabra <david.a.khazzam@gmail.com>
2022-11-01 15:07:37 -0400

    made main view a borden pane, content grows

--- frontend/src/main/kotlin/TODOApplication.kt
+++ frontend/src/main/kotlin/TODOApplication.kt
@@ -4,7 +4,7 @@ import controllers.GroupViewController
 import controllers.SidepaneController
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.layout.*
+import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 import models.WindowSettings
 import views.GroupView
