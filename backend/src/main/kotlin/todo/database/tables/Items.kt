a206f72ca9385df2d86a51cf469e5e611ec20931
upsicleclown <roxiestanciu12@gmail.com>
2022-11-01 19:58:29 -0400

    Add priority field to Item

--- backend/src/main/kotlin/todo/database/tables/Items.kt
+++ backend/src/main/kotlin/todo/database/tables/Items.kt
@@ -1,6 +1,9 @@
 package todo.database.tables
 
+import models.Priority
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.compoundOr
 import org.jetbrains.exposed.sql.javatime.datetime
 
 /**
@@ -10,4 +13,5 @@ object Items : IntIdTable() {
     val title = varchar("title", 50)
     val isCompleted = bool("is_completed")
     val edtDueDate = datetime(name = "edtDueDate").nullable()
+    val priority = varchar("priority", 25).check { (Priority.values().map { priorityEnum -> Op.build { it eq priorityEnum.name } } + it.isNull()).compoundOr() }.nullable()
 }
