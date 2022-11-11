aa075c624471fd68327e9d926f8b00b21d3f2f40
Moe Amadou <moeamadou@Moes-MacBook-Pro.local>
2022-11-11 14:51:55 -0500

    Add logic for pre-existing vs. new labels in CreateItemLabelCommand

--- frontend/src/main/kotlin/commands/CreateItemLabelCommand.kt
+++ frontend/src/main/kotlin/commands/CreateItemLabelCommand.kt
@@ -5,13 +5,21 @@ import models.Item
 import models.Label
 
 class CreateItemLabelCommand(
-    private val label: Label,
+    private val existingLabel: Boolean,
+    private val newLabel: Label,
     private val item: Item,
     private val controller: GroupViewController
 ) : Command {
     override fun execute() {
         var newItem = item.copy()
-        newItem.labelIds.add(label.id)
+
+        if (existingLabel) {
+            newItem.labelIds.add(newLabel.id)
+        } else {
+            controller.createLabel(newLabel)
+            var newLabelRecord = controller.labels().last { label -> label.name == newLabel.name }
+            newItem.labelIds.add(newLabelRecord.id)
+        }
 
         controller.editItem(newItem = newItem, originalItem = item)
         controller.reloadGroupView()
@@ -19,7 +27,13 @@ class CreateItemLabelCommand(
 
     override fun undo() {
         var newItem = item.copy()
-        newItem.labelIds.remove(label.id)
+
+        if (existingLabel) {
+            newItem.labelIds.remove(newLabel.id)
+        } else {
+            var newLabelRecord = controller.labels().last { label -> label.name == newLabel.name }
+            newItem.labelIds.remove(newLabel.id)
+        }
 
         controller.editItem(newItem = newItem, originalItem = item)
         controller.reloadGroupView()
