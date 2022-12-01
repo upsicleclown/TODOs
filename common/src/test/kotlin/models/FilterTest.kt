d59150bd5381feb9f54f16b6097adae8d780f5da
upsicleclown <roxiestanciu12@gmail.com>
2022-11-30 19:59:07 -0500

    Add tests

--- common/src/test/kotlin/models/FilterTest.kt
+++ common/src/test/kotlin/models/FilterTest.kt
@@ -10,12 +10,14 @@ internal class FilterTest {
     fun testConstructor() {
         val expectedEdtStartDate = LocalDateTime.parse("2010-06-01T22:19:44")
         val expectedEdtEndDate = LocalDateTime.parse("2016-06-01T22:19:44")
+        val expectedLabelBooleanOperator = BooleanOperator.AND
         val expectedIsCompleted = false
 
-        val filter = Filter(expectedEdtStartDate, expectedEdtEndDate, expectedIsCompleted)
+        val filter = Filter(expectedEdtStartDate, expectedEdtEndDate, expectedIsCompleted, labelBooleanOperator = expectedLabelBooleanOperator)
         assertEquals(filter.edtStartDateRange, expectedEdtStartDate)
         assertEquals(filter.edtEndDateRange, expectedEdtEndDate)
         assertEquals(filter.edtEndDateRange, expectedEdtEndDate)
+        assertEquals(filter.labelBooleanOperator, expectedLabelBooleanOperator)
         assertEquals(filter.labelIds, listOf<Int>())
         assertEquals(filter.priorities, listOf<Priority>())
     }
